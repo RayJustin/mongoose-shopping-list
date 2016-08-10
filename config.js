06c555057914e66a6b01a29e4eb249d322c43424
RayJustin <Ray.Justin@live.com>
2016-08-10 16:31:40 -0700

    added Travis CLI

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || (process.env.NODE_ENV === 'production' ? 'mongodb://<dbuser>:<dbpassword>@ds153735.mlab.com:53735/justins_database' : 'mongodb://localhost/shopping-list-dev');
+exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || (process.env.NODE_ENV === 'production' ? 'mongodb://<dbuser>:<dbpassword>@ds153735.mlab.com:53735/justins_database' : 'mongodb://Justin:Guitar36@ds153735.mlab.com:53735/justins_database');
 
 exports.PORT = process.env.PORT || 8080;
